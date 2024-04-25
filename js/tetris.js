bb224ce92ed59452af9dad59c2c35e7ff236e9d9
Jayeon Hwang <jayeon512@gmail.com>
2024-04-25 17:14:29 -0500

    setup variables and functions

--- js/tetris.js
+++ js/tetris.js
@@ -1,14 +1,39 @@
+//DOM
 const playground = document.querySelector(".playground > ul");
 
-console.log(playground);
+// Setting
 
-for (let i = 0; i < 20; i++) {
+const GameRows = 20;
+const GameCols = 10;
+
+// Variables 
+let score = 0;
+let duration = 500;
+let downInterval;
+let tempMovingItem;
+const movingItem = {
+  type: "",
+  direction: 0,
+  top: 0,
+  left: 0,
+};
+
+init();
+
+//Functions 
+function init() {
+  for (let i = 0; i < GameRows; i++) {
+    prependNewLine();
+  }
+}
+
+function prependNewLine() {
   const li = document.createElement("li");
   const ul = document.createElement("ul");
-  for (let j = 0; j < 10; j++) {
+  for (let j = 0; j < GameCols; j++) {
     const matrix = document.createElement("li");
     ul.prepend(matrix);
   }
-  li.prepend(ul)
-  playground.prepend(li)
+  li.prepend(ul);
+  playground.prepend(li);
 }
